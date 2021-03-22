95926b186de37f211a83f009076fe7c68528c7fe
skipper8 <mharper48@gmail.com>
2021-03-22 14:14:30 -0700

    comments

--- PizzaBox.py
+++ PizzaBox.py
@@ -1,20 +1,29 @@
-<<<<<<< HEAD
+#Libraries______________________________________________________________
+
 import discord
 import numpy as np
 from discord.ext import commands
 import random
 
+#Set Up bot____________________________________________________________
 bot = commands.Bot(command_prefix = '$')
 rules = []
 prules = []
 fav = []
+
+#Check________________________________________________________________
+#checks if the author of a message is a bot
+
 def check(author):
     def inner_check(message):
         return ctx.message.author.bot
+
+#Anounce Arrival______________________________________________________
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
+#Hello_______________________________________________________________
 @bot.event
 async def on_message(message):
     if message.author == bot.user: return #Makes sure it can't loop itself when making messages
@@ -22,27 +31,28 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         channel = message.channel
         await channel.send('Hello {}!'.format(message.author.name))
-        print("ran")
 
+#Begin______________________________________________________________
 @bot.command(alliaces = ['go', 'start', 'commence'])
 async def begin(ctx):
+    #Clears prior player and rule history
     rules.clear()
     prules.clear()
     players = []
+    #prevents bots from playing
     for i in bot.users:
         if i.bot == False:
             players.append(i)
+    #adds player to roster and gives them a space
     for p in players:
         x = 70*players.index(p)+35
         y = x//840
         pt = [30, 70*y + 35, x - 840*y]
-        print(pt)
         rules.append([pt,p.name])
         prules.append(p.name)
-    print(rules)
     await ctx.send('I have perpared the Pizza Box')
             
-        
+#Flip_______________________________________________________________        
 @bot.command(alliases = ['flip', 'test'])
 async def flip(ctx):
     await ctx.send('Pick a number between 1-840')
@@ -77,22 +87,12 @@ async def flip(ctx):
     m_rad = min(x, y, 420-y, 840-x)
     for r in rules:
         pt = r[0]
-        print(pt)
         radius = pt[0]
-        print(radius)
         pt_y = pt[1]
-        print(pt_y)
         pt_x = pt[2]
-        print(pt_x)
-        print(y)
-        print(x)
         y_diff= pt_y - y
-        print(y_diff)
         x_diff = x-pt_x
-        print(x_diff)
-        print(x_diff**2)
         distance = np.sqrt(float((y - pt_y)**2+(x-pt_x)**2))
-        print(distance)
         if distance <= radius:
             await ctx.send(r[1])
             z = 1;
@@ -121,9 +121,9 @@ async def flip(ctx):
         rules.append([point, rule])
         prules.append(rule)
         await ctx.send('I have your rule')
-            
-    print("ran")
-    
+
+
+#Add Favorites    
 @bot.command(alliases=['add like','add ideas','add fav', 'add'])
 async def add_favorite(ctx):
     await ctx.send('What rule would you like to add to favorites?')
@@ -132,14 +132,17 @@ async def add_favorite(ctx):
     fav.append(f)
     await ctx.send('Rule has been added')
 
+#Tell Favorites
 @bot.command(alliases=['ideas', 'like', 'fav'])
 async def favorite(ctx):
     await ctx.send(fav)
 
+#Tell Rules
 @bot.command()
 async def what_rules(ctx):
     await ctx.send(prules)
 
+#End Game
 @bot.command()
 async def end_game(ctx):
     rules.clear()
